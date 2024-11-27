d1832f94e61f4f2e19d1cc2ef92de0f6cc9dd451
Thanusha <110088494+Thanusha-rani@users.noreply.github.com>
2024-11-27 17:57:48 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Thanusha";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/hero1.png");
         }
         else {
             document.title = "Come Back To Portfolio";
